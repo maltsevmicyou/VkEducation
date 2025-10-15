d823580c2826dd4f696cf89dcef3c475d8187a56
michael.maltsev <michael.maltsev@vk.team>
2025-10-15 16:47:07 +0700

    Добавил экран AppDetails

--- app/src/main/java/io/mmaltsev/vkeducation/MainActivity.kt
+++ app/src/main/java/io/mmaltsev/vkeducation/MainActivity.kt
@@ -5,12 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import io.mmaltsev.vkeducation.ui.theme.VkEducationTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +15,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             VkEducationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AppDetailsScreen(
+                    Modifier
+                        .fillMaxSize()
+                        .safeDrawingPadding()
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    VkEducationTheme {
-        Greeting("Android")
     }
 }
